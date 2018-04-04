221e803546c4452e27213f4e6fcb3444dfa6d457
Justin F <fujs@uci.edu>
2018-04-04 14:14:35 -0700

    Update Coin Change Problem.py

--- Coin Change Problem.py
+++ Coin Change Problem.py
@@ -1,56 +1,34 @@
-from collections import defaultdict
-# 4, [1,2,3]
-#[[1,1,1,1],[1,1,2],[2,2],[1,3]]
+#===============================
+# Problem:   Given a target integer and a list of integers, find all possible
+#                     combinations of the list of integers that sum up the target.
+#===============================
 
-## Test case 1
-goal = 10
-test = [2,3,5,6]
-test2 = [2,3,5,6]
 
-memoize = defaultdict(list)
-
-#correct numbers, but how to store them when retrning up the recursion....
-# is this dynamic programming???
+# ===============================
+#  Recursion
+# ===============================
 
 def get_ways( total: int, values: list ):
         
-        memoize = defaultdict(list)
+        answer = []
         
-        values = sorted(values)
-        print(values)
-        def fill_memoize( total:int, values: list, curr: int):
-                print(memoize, total, curr)
-                input("...")
+        def find_ways( total:int, values: list, curr: list):
+        # curr = list of current value used to reduce total
                 final = []
                 if total == 0:
-                        return curr
+                        new_curr = sorted(curr, reverse = True)
+                        if new_curr not in answer:
+                                answer.append(new_curr)
                 
-                elif total in memoize.keys():
-                        looped = []
-                        print("in")
-                        for index, item in enumerate(memoize[total]):
-                                print("in", looped, total, item)
                 else:
-                        looped = []
                         for index, item in enumerate(values):
                                 result = total - item
                                 if result >= 0:
-                                        memoize[total].append(total-item)
-                                        looped.append(fill_memoize(result, values, item))
-                                        print("not in", looped, total, item)
-                                print("looped", looped)
-                                final += looped
-                        print("final", final)
-                        return final
+                                        new_curr = curr + [item]
+                                        find_ways(result, values, new_curr)                                
 
+        find_ways(total, values, [])
 
-        result = fill_memoize(total, values, total)
+        return answer
 
-        #set(item for item in result)
-        print(memoize)
-        return result
-
-check = get_ways(10, [2,5,3,6])
-print(check)
-
-
+print(get_ways(10,[2,5,3,6]))
