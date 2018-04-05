91531360ce0fa6baf0b3b7d9d5dfcf1073ec77b3
Justin F <fujs@uci.edu>
2018-04-05 11:56:31 -0700

    Update Coin Change Problem.py

--- Coin Change Problem.py
+++ Coin Change Problem.py
@@ -8,27 +8,73 @@
 #  Recursion
 # ===============================
 
-def get_ways( total: int, values: list ):
+def get_ways_r( total: int, values: list ):
         
         answer = []
-        
+        count = []
         def find_ways( total:int, values: list, curr: list):
         # curr = list of current value used to reduce total
+        
                 final = []
                 if total == 0:
                         new_curr = sorted(curr, reverse = True)
                         if new_curr not in answer:
                                 answer.append(new_curr)
+                                #total = 0 so we append the curr into the answer list
                 
                 else:
                         for index, item in enumerate(values):
                                 result = total - item
                                 if result >= 0:
                                         new_curr = curr + [item]
-                                        find_ways(result, values, new_curr)                                
+                                        count.append(1)
+                                        find_ways(result, values, new_curr)
+                                        #recursion: result = recuded total; new_curr = curr with item appended
 
         find_ways(total, values, [])
+        
+        return len(count), answer
 
-        return answer
+# ===============================
+#  Memoization
+#       create a default dict
+#       the keys = the totals
+#       values = list of curr that are called along the total
+#       Before each call, check if the total, and the curr exist, if they do, skip.
+# ===============================
+from collections import defaultdict
 
-print(get_ways(10,[2,5,3,6]))
+def get_ways_m( total: int, values: list ):
+        answer = []
+        memoize = defaultdict(list)
+        #new data structure to keep account of all {total:[curr]} occurences
+        count = []
+
+        def find_ways(total: int, values: list, curr: list):
+                final = []
+                if total == 0:
+                        new_curr = sorted(curr, reverse = True)
+                        if new_curr not in answer:
+                                memoize[total].append(new_curr)
+                                #add the answer into the memoize
+                                #all answers should be in key = 0
+                                answer.append(new_curr)
+
+                else:
+                        for index, item in enumerate(values):
+                                result = total - item
+                                if result >= 0:
+                                        new_curr = sorted(curr + [item])
+                                        if new_curr not in memoize[total]:
+                                                #check memoize
+                                                memoize[total].append(new_curr)
+                                                count.append(1)
+                                                find_ways(result, values, new_curr)
+                
+        find_ways(total, values, [])
+        return len(count), answer
+
+
+print(get_ways_r(10, [2,5,3,6]))
+print(get_ways_m(10, [2,5,3,6]))
+                 
