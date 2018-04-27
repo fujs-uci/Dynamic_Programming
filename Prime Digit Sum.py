8ad897d5b66064667a911d988a6607ebd358ae96
Justin F <fujs@uci.edu>
2018-04-27 01:05:49 -0700

    Update Prime Digit Sum.py

--- Prime Digit Sum.py
+++ Prime Digit Sum.py
@@ -10,7 +10,21 @@ y = [3,4,5]
 n - (y -1) = number of possible combinations
 
 """
+        
+def isPrime(n):
+        prime = True
+        check = math.sqrt(n)
 
+        for i in range(2,math.ceil(check)+1):
+                if n % i == 0:
+                        prime = False
+                        break
+        return prime
+
+def indexInt(n):
+        #turns an int into a list of ints
+        index = [int(i) for i in str(n)]
+        return index
 
 def primeDigitSums(n):
         start = (1*(10**(n-1)))
@@ -22,38 +36,36 @@ def primeDigitSums(n):
                 #for digit in [3,4,5]
                         #for every possibility
                                 #check sum = prime
-        for i in range(start, end):
-                break
-        print(isPrime(n))
-        print(indexInt(n))
-        
-def isPrime(n):
-        prime = True
-        check = math.sqrt(n)
-        if math.ceil(check) == check:
-                return False
-        for i in range(2,math.ceil(check)):
-                if n % i == 0:
-                        prime = False
-                        break
-        return prime
 
-def indexInt(n):
-        #turns an int into a list of ints
-        index = [int(i) for i in str(n)]
-        return index
+        #3,4,5 property only applies to lengths that it applies to.
+        ans = []
+        pass
+
+def checkSet(n: list):
+
+        #p_sum = previous sum, first sum of the array
+        #minus the first number, add the last number of increasing length subsets
+        # add list[index] to previous sum
+
+        def checkSub( p_sum: int, n: list, numb: int):
+
+                for numb in range(len(n)):
+                        break
+
+        pass
+                        
+        
+        
+
 
 if __name__ == "__main__":
         #print(primeDigitSums(6))
 ##        for i in range(6):
 ##                print(primeDigitSums(i)
-        primes = []
-        for i in range(1000):
-                if isPrime(i):
-                        primes.append(i)
 
-        primeDigitSums(6)
-
+        #primeDigitSums(6)
+        print(isPrime(121))
+                        
 
 
 
